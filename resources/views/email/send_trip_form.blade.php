abdd85ccba994d4e28fb2751176db6e52ffbc7af
Siddharth <siddharth@gteches.com>
2023-08-22 16:12:29 +0530

    cron job url update

--- resources/views/email/send_trip_form.blade.php
+++ resources/views/email/send_trip_form.blade.php
@@ -13,7 +13,7 @@
     <p>Thank you for visiting the trip. <br> Hope you have enjoyed the best of it.</p>
     <p>It will be great if you can share a valuable feedback, <br> Please by fill the feedback form, so we can keep improving our service</p>
     <p>Click on below button to fill the form. </p>
-    <a href="https://websitesample.in/POC/trip_form/{{$user_type}}/{{$trip_id}}/{{$details->id}}">Feedback</a>
+    <a href="https://poc.mytravel2.com/trip_form/{{$user_type}}/{{$trip_id}}/{{$details->id}}">Feedback</a>
     <p>Thank you</p>
 
 </body>
